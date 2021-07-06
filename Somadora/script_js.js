7c2a55a9a88f4bce1a2b79ca58aac6bc9ca46fb8
legovirtual <86530090+legovirtual@users.noreply.github.com>
2021-07-05 21:43:22 -0300

    js 2.0
    Um pouco mais complexo e bem feito

--- Somadora/script_js.js
+++ Somadora/script_js.js
@@ -2,6 +2,15 @@ function cal(){
     let t = document.getElementById('text');
     let n1 = document.getElementById('in1').value;
     let n2 = document.getElementById('in2').value;
+    t.textContent = ''
+    if(n1 == ''){
+        t.textContent = 'Digite um numero para fazer a soma'
+    }
+    else if(n2 == ''){
+        t.textContent = 'Digite um numero para fazer a soma'
+    }
+    else{
     let ca = parseInt(n1)+parseInt(n2)
-    t.textContent = `A soma dos numeros: ${n1} mais ${n2} é = ${ca}`
+    t.textContent = `A soma dos numeros: ${n1} + ${n2} é = ${ca}`
+    }
 }
